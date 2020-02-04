9f435e653389fb883f95af70aaef30dcdce4c2b1
Adboessen <andrewboessen@amdg.rockhursths.edu>
2020-02-04 08:44:19 -0600

    some code

---
+++ HelloGit.py
@@ -0,0 +1,54 @@
+print("hello world")
+a= 5
+b =6
+
+# how to make a list
+myList = [1,2,3,45,3,2]
+
+# how to stride
+print(myList[0:6:2]) # print from index 0 - 6 by twos
+
+# strings are just lists
+phrase = 'Catch the dog'
+phrase[2] #t
+
+# how to access list
+print(myList[1])
+
+# how to call a function
+def sumFunction(a,  b):
+return a + b
+
+print(sunFunction(2,20))
+
+x = 10
+y = -10
+
+# blocks of code
+if(x == 10)
+	x= 5
+elif(y == -10)
+	y =5
+else:
+	x = y - x
+
+#loop
+myList = [1,2,3,8,412]
+
+# length of list
+len(myList)
+
+# for loop by index
+for i in range(len(myList)):
+	print(myList[i])
+
+# for each loop
+for val in myList:
+	print(val)
+
+# dictionaries
+lookup = {}
+lookup['kc'] = 'cheifs'
+lookup['ne'] = 'patriots'
+lookup['la'] = 'chargers'
+lookup['suck'] = 'radiers'
